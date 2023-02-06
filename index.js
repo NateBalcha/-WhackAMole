f2762ff32910f0ac27209d905cad3b839b370de3
Natnael Balcha <natnaelbalcha@Natnaels-MacBook-Pro.local>
2023-02-05 23:54:29 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const holes = document.querySelectorAll(".hole");
+const scoreBoard = document.querySelector(".score");
+const moles = document.querySelectorAll(".mole");
+let lastHole;
+
+function randomTime(min, max){
+    return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomHole(holes){
+    const index = Math.floor(Math.random() * holes.length);
+    const hole = holes[index];
+    if(hole === lastHole) {
+        console.log("repatted");
+        return randomHole(holes);
+    }
+    lastHole = hole;
+    return hole;
+}
